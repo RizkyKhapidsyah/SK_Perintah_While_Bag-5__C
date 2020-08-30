cee6ce1b23f466fbf4ee4a37d412d1388114595e
RizkyKhapidsyah <RizkyKhapidsyah@RIKY_PC>
2020-08-30 13:32:33 +0700

    Add project files.

---
+++ Source.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+	int a,b;
+
+	a = 7;
+	while( a < 30 )
+	{
+		b = a % 7;		/* b equals a mod 7 */
+		printf("%d %% 7 = %d\n",a,b);
+		a++;
+	}
+
+	_getch();
+	return(0);
+}
